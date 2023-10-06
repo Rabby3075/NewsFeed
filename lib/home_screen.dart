832d6077d8b0ffeafd4ca62790dcfa44e71c20ee
Rabby3075 <rashed.rabby43@gmail.com>
2023-10-07 00:42:02 +0600

    potrait

---
+++ lib/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class HomeScreen extends StatelessWidget {
+   HomeScreen({super.key});
+
+   Widget _PortraitLayout(){
+     return ListView.builder(
+       itemCount: 10,
+       itemBuilder: (context, index) {
+         return Column(
+           children: [
+             Card(
+               elevation: 4, // Add elevation (shadow) here
+               shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.circular(5), // Customize the border radius
+               ),
+               child: Container(
+                 width: MediaQuery.of(context).size.width,
+                 height: 150,
+                 child: Center(
+                   child: Image.network(
+                     'https://via.placeholder.com/150',
+                     fit: BoxFit.cover,
+                   ),
+                 ),
+               ),
+             ),
+             SizedBox(height: 16.0), // Adjust the height as needed for the gap
+           ],
+         );
+       },
+     );
+   }
+   Widget _LandscapeLayout(){
+     return Column(
+
+     );
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Feed'),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return orientation == Orientation.portrait
+              ? _PortraitLayout()
+              : _LandscapeLayout();
+        },
+      )
+    );
+  }
+}
